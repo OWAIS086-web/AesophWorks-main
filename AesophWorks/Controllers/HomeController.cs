87d9146b44b4aeea9af4060c38765a1433002577
Qazi Mubin <qazimubin444@gmail.com>
2022-05-03 16:41:36 +0500

    Login

--- AesophWorks/Controllers/HomeController.cs
+++ AesophWorks/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AesophWorks.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -31,6 +32,40 @@ namespace AesophWorks.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            var user = UserServices.Instance.GetUserForLogin(username, password);
+            if (user != null)
+            {
+                Session["ID"] = user.ID.ToString();
+                Session["UserName"] = user.UserName.ToString();
+                Session["Email"] = user.Email.ToString();
+                Session["Role"] = user.Role.ToString();
+                if (user.Role == "Admin")
+                {
+                    return RedirectToAction("Dashboard", "Admin");
+                }
+                else if (user.Role == "User")
+                {
+                    return RedirectToAction("Dashboard", "User");
+                }
+                else
+                {
+                    string msg = "Invalid Password or UserName";
+                    TempData["ErrorMessage"] = msg;
+                    Session["ID"] = null;
+                    Session["UserName"] = null;
+                    Session["Email"] = null;
+                    Session["Role"] = null;
+
+                }
+
+            }
+            return RedirectToAction("Login", "Home");
         }
     }
 }
