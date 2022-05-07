c3a4543bffdc84aa4acd8d547e880e73e4e43b20
Qazi Mubin <qazimubin444@gmail.com>
2022-05-07 13:08:29 +0500

    Basic Order screen

---
+++ AesophWorks/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using AesophWorks.Services;
+using AesophWorks.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AesophWorks.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+
+        public ActionResult MakeOrder()
+        {
+            MakeOrderViewModel model = new MakeOrderViewModel();
+            model.Items = ItemServices.Instance.GetAllItems("");
+            return View(model);
+        }
+
+
+    }
+}
---
+++ AesophWorks/ViewModels/MakeOrderViewModel.cs
@@ -0,0 +1,18 @@
+﻿using AesophWorks.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AesophWorks.ViewModels
+{
+    public class MakeOrderViewModel
+    {
+        public List<Item> Items { get; set; }
+        public List<Size> Sizes { get; set; }
+        public List<CutterButter> CutterButters { get; set; }
+        public List<Feet> Feets { get; set; }
+        public List<Inlay> Inlays { get; set; }
+        public List<WoodType> WoodTypes { get; set; }
+    }
+}
